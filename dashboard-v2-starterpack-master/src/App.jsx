5e01bfe9ed74f648e131b1d5df13c22e0b3fd79d
karol cardona <kfcardonaaponte14@gmail.com>
2023-11-14 00:41:24 -0500

    Segundo commit

--- dashboard-v2-starterpack-master/src/App.jsx
+++ dashboard-v2-starterpack-master/src/App.jsx
@@ -1,5 +1,19 @@
+import {BrowserRouter, Router, Routers} from 'react-router-dom'
+import Dashboard from './pages/Dashboard/Dashboard';
+
+
 const App = () => {
-  return <span>Subscribe Zain keepscode</span>;
+  return <div id="dashboard">
+    <BrowserRouter>
+    <Routers>
+      <Router path="/" element={<Layout/>}>
+
+      <Router path="dashboard" element={<Dashboard/>}/>
+
+      </Router>
+    </Routers>
+    </BrowserRouter>
+  </div>
 };
 
 export default App;
---
+++ dashboard-v2-starterpack-master/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import CSS from './Dashboard.css';
+
+const Dashboard = () => {
+    return(
+        <div>Dashboard</div>
+    )
+}
+
+export default Dashboard
